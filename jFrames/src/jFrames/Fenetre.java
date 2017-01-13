9e882bdcd3a18078e78b9e0936a977636784e2f1
Alexandre SCHANNE <alexandre.schanne@telecomanancy.eu>
2017-01-13 10:32:30 +0100

    jPanel + boutons + listes

--- jFrames/src/jFrames/Fenetre.java
+++ jFrames/src/jFrames/Fenetre.java
@@ -1,71 +1,101 @@
 package jFrames;
 
-import java.awt.Dimension; 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+ 
 public class Fenetre extends JFrame{
-	private Panneau pan = new Panneau();
+  private Panneau pan = new Panneau();
+  private JButton bouton = new JButton("Go");
+  private JButton bouton2 = new JButton("Stop");
+  private JPanel container = new JPanel();
+  private JLabel label = new JLabel("Le JLabel");
+  private int compteur = 0;
+  private boolean animated = true;
+  private boolean backX, backY;
+  private int x, y;
+  private Thread t;
 
-	public Fenetre(){        
-		this.setTitle("Animation");
-		this.setSize(300, 300);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLocationRelativeTo(null);
-		this.setContentPane(pan);
-		this.setVisible(true);
-		go();
-	}
+  public Fenetre(){
+    this.setTitle("Animation");
+    this.setSize(300, 300);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLocationRelativeTo(null);
 
-	private void go(){
-		//Les coordonnées de départ de notre rond
-		int x = pan.getPosX(), y = pan.getPosY();
-		//Le booléen pour savoir si l'on recule ou non sur l'axe x
-		boolean backX = false;
-		//Le booléen pour savoir si l'on recule ou non sur l'axe y
-		boolean backY = false;
+    container.setBackground(Color.white);
+    container.setLayout(new BorderLayout());
+    container.add(pan, BorderLayout.CENTER);
+    bouton.addActionListener(new BoutonListener()); 
+    bouton.setEnabled(false);
+    bouton2.addActionListener(new Bouton2Listener());
 
-		//Dans cet exemple, j'utilise une boucle while
-		//Vous verrez qu'elle fonctionne très bien
-		while(true){
-			//Si la coordonnée x est inférieure à 1, on avance
-			if(x < 1)
-				backX = false;
+    JPanel south = new JPanel();
+    south.add(bouton);
+    south.add(bouton2);
+    container.add(south, BorderLayout.SOUTH);
+    Font police = new Font("Tahoma", Font.BOLD, 16);
+    label.setFont(police);
+    label.setForeground(Color.blue);
+    label.setHorizontalAlignment(JLabel.CENTER);
+    container.add(label, BorderLayout.NORTH);
+    this.setContentPane(container);
+    this.setVisible(true);
+    go();
+  }
 
-			//Si la coordonnée x est supérieure à la taille du Panneau moins la taille du rond, on recule
-			if(x > pan.getWidth()-50)
-				backX = true;
+  private void go(){
+    //Les coordonnées de départ de notre rond
+    x = pan.getPosX();
+    y = pan.getPosY();
+    //Dans cet exemple, j'utilise une boucle while
+    //Vous verrez qu'elle fonctionne très bien
+    while(this.animated){
+      if(x < 1)backX = false;
+      if(x > pan.getWidth()-50)backX = true;          
+      if(y < 1)backY = false;
+      if(y > pan.getHeight()-50)backY = true;
+      if(!backX)pan.setPosX(++x);
+      else pan.setPosX(--x);
+      if(!backY) pan.setPosY(++y);
+      else pan.setPosY(--y);
+      pan.repaint();
 
-			//Idem pour l'axe y
-			if(y < 1)
-				backY = false;
-			if(y > pan.getHeight()-50)
-				backY = true;
+      try {
+        Thread.sleep(3);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }     
+  }
 
-			//Si on avance, on incrémente la coordonnée
-			//backX est un booléen, donc !backX revient à écrire
-			//if (backX == false)
-			if(!backX)
-				pan.setPosX(++x);
+  public class BoutonListener implements ActionListener{
+	    public void actionPerformed(ActionEvent arg0) {
+	      animated = true;
+	      t = new Thread(new PlayAnimation());
+	      t.start();
+	      bouton.setEnabled(false);
+	      bouton2.setEnabled(true);
+	    }
+	  }
 
-			//Sinon, on décrémente
-			else
-				pan.setPosX(--x);
+	  class Bouton2Listener  implements ActionListener{
+	    public void actionPerformed(ActionEvent e) {
+	      animated = false;       
+	      bouton.setEnabled(true);
+	      bouton2.setEnabled(false);
+	    }
+	  }       
 
-			//Idem pour l'axe Y
-			if(!backY)
-				pan.setPosY(++y);
-			else
-				pan.setPosY(--y);
-
-			//On redessine notre Panneau
-			pan.repaint();
-
-			//Comme on dit : la pause s'impose ! Ici, trois millièmes de seconde
-			try {
-				Thread.sleep(3);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}     
+	  class PlayAnimation implements Runnable{
+	    public void run() {
+	      go();                   
+	    }               
+	  }       
 }
